5a3ce652ac88677e5419a6f1a9e58c834f31211a
yagi-web <223397855+yagi-web@users.noreply.github.com>
2025-12-23 20:29:13 +0900

    RoomSeederを削除、部屋情報と部屋画像シーダーを新規作成

--- database/seeders/RoomImagesTableSeeder.php
+++ database/seeders/RoomImagesTableSeeder.php
@@ -12,65 +12,65 @@ class RoomImagesTableSeeder extends Seeder
     {
         RoomImage::truncate();
 
-        // --- スタンダードルーム (ID: 1) 用の画像5枚 ---
+        // --- プレミアムルーム (ID: 1) 用の画像5枚 ---
         RoomImage::create([
             'room_id'    => 1,
-            'image_url'  => 'https://placehold.jp/24/555555/ffffff/800x600.png?text=Standard_1',
+            'image_url'  => 'https://i.postimg.cc/mgvW8gz5/premium1.jpg',
             'sort_order' => 1,
         ]);
 
         RoomImage::create([
             'room_id'    => 1,
-            'image_url'  => 'https://placehold.jp/24/555555/ffffff/800x600.png?text=Standard_2',
+            'image_url'  => 'https://i.postimg.cc/FK05qX3W/premium2.jpg',
             'sort_order' => 2,
         ]);
 
         RoomImage::create([
             'room_id'    => 1,
-            'image_url'  => 'https://placehold.jp/24/555555/ffffff/800x600.png?text=Standard_3',
+            'image_url'  => 'https://i.postimg.cc/hGVWwRTZ/premium3.jpg',
             'sort_order' => 3,
         ]);
 
         RoomImage::create([
             'room_id'    => 1,
-            'image_url'  => 'https://placehold.jp/24/555555/ffffff/800x600.png?text=Standard_4',
+            'image_url'  => 'https://i.postimg.cc/SKcF5bCP/premium4.jpg',
             'sort_order' => 4,
         ]);
 
         RoomImage::create([
             'room_id'    => 1,
-            'image_url'  => 'https://placehold.jp/24/555555/ffffff/800x600.png?text=Standard_5',
+            'image_url'  => 'https://i.postimg.cc/cJfyjGwb/premium5.jpg',
             'sort_order' => 5,
         ]);
 
         // --- スタンダードルーム (ID: 2) 用の画像5枚 ---
         RoomImage::create([
             'room_id'    => 2,
-            'image_url'  => 'https://placehold.jp/24/555555/ffffff/800x600.png?text=Standard_1',
+            'image_url'  => 'https://i.postimg.cc/5t8dGW8c/standard1.jpg',
             'sort_order' => 1,
         ]);
 
         RoomImage::create([
             'room_id'    => 2,
-            'image_url'  => 'https://placehold.jp/24/555555/ffffff/800x600.png?text=Standard_2',
+            'image_url'  => 'https://i.postimg.cc/hGVWwRTk/standard2.jpg',
             'sort_order' => 2,
         ]);
 
         RoomImage::create([
             'room_id'    => 2,
-            'image_url'  => 'https://placehold.jp/24/555555/ffffff/800x600.png?text=Standard_3',
+            'image_url'  => 'https://i.postimg.cc/Pq1scn1T/standard3.jpg',
             'sort_order' => 3,
         ]);
 
         RoomImage::create([
             'room_id'    => 2,
-            'image_url'  => 'https://placehold.jp/24/555555/ffffff/800x600.png?text=Standard_4',
+            'image_url'  => 'https://i.postimg.cc/3w256Q2J/standard4.jpg',
             'sort_order' => 4,
         ]);
 
         RoomImage::create([
             'room_id'    => 2,
-            'image_url'  => 'https://placehold.jp/24/555555/ffffff/800x600.png?text=Standard_5',
+            'image_url'  => 'https://i.postimg.cc/Zqs7VfMt/standard5.png',
             'sort_order' => 5,
         ]);
     }
